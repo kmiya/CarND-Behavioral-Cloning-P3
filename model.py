9ac6c135c7fbbaa997c986fee891a89f6345cf7f
kmiya <kmiya@outlook.com>
2020-12-20 18:13:04 +0900

    change the names of datasets

--- model.py
+++ model.py
@@ -91,9 +91,9 @@ def define_model(input_shape):
 
 
 def main():
-    samples_c1_2 = get_csv_data('./course1_2/driving_log.csv')
-    samples_c1_3 = get_csv_data('./course1_3/driving_log.csv')
-    samples = samples_c1_2 + samples_c1_3
+    samples_c1_kb = get_csv_data('./course1_kb/driving_log.csv')
+    samples_c1_mouse = get_csv_data('./course1_mouse/driving_log.csv')
+    samples = samples_c1_kb + samples_c1_mouse
     train_samples, validation_samples = train_test_split(
         samples, test_size=0.2)
     epochs = 5
